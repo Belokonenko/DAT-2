bc8e33fb7c21064222320a16fed1f9b2bb60be22
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-11-25 04:30:26 +0200

    Rename : secect-menu

--- src/components/bem-blocks/drop-menu/d-m-002/d-m-002.js
+++ src/components/bem-blocks/drop-menu/d-m-002/d-m-002.js
@@ -1,8 +1,8 @@
 export default function dM002() {
     const optionMenu = document.querySelector(".select-menu"),
-        selectBtn = optionMenu.querySelector(".select-btn"),
-        options = optionMenu.querySelectorAll(".option"),
-        sBtn_text = optionMenu.querySelector(".sBtn-text");
+        selectBtn = optionMenu.querySelector(".select-menu__btn"),
+        options = optionMenu.querySelectorAll(".select-menu__option"),
+        sBtn_text = optionMenu.querySelector(".select-menu__btn-text");
 
     selectBtn.addEventListener("click", () =>
         optionMenu.classList.toggle("active")
@@ -10,7 +10,7 @@ export default function dM002() {
 
     options.forEach((option) => {
         option.addEventListener("click", () => {
-            let selectedOption = option.querySelector(".option-text").innerText;
+            let selectedOption = option.querySelector(".select-menu__option-txt").innerText;
             sBtn_text.innerText = selectedOption;
 
             optionMenu.classList.remove("active");
