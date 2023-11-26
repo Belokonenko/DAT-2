d1028c0be946dfed9025b57c73d281d974e5a09d
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-11-26 10:14:56 +0200

    Fix : select-menu.js

--- src/components/bem-blocks/drop-menu/select-menu/select-menu.js
+++ src/components/bem-blocks/drop-menu/select-menu/select-menu.js
@@ -1,19 +1,24 @@
 export default function selctMenu() {
-    const optionMenu = document.querySelector(".select-menu"),
-        selectBtn = optionMenu.querySelector(".select-menu__btn"),
-        options = optionMenu.querySelectorAll(".select-menu__option"),
-        sBtn_text = optionMenu.querySelector(".select-menu__btn-text");
+    const optionMenu = document.querySelector(".select-menu");
+    
+    if (optionMenu) {
+        const selectBtn = optionMenu.querySelector(".select-menu__btn"),
+            options = optionMenu.querySelectorAll(".select-menu__option"),
+            sBtn_text = optionMenu.querySelector(".select-menu__btn-text");
 
-    selectBtn.addEventListener("click", () =>
-        optionMenu.classList.toggle("active")
-    );
+        selectBtn.addEventListener("click", () =>
+            optionMenu.classList.toggle("active")
+        );
 
-    options.forEach((option) => {
-        option.addEventListener("click", () => {
-            let selectedOption = option.querySelector(".select-menu__option-txt").innerText;
-            sBtn_text.innerText = selectedOption;
+        options.forEach((option) => {
+            option.addEventListener("click", () => {
+                let selectedOption = option.querySelector(
+                    ".select-menu__option-txt"
+                ).innerText;
+                sBtn_text.innerText = selectedOption;
 
-            optionMenu.classList.remove("active");
+                optionMenu.classList.remove("active");
+            });
         });
-    });
+    }
 }
