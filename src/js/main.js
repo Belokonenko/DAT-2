f1539bdf57bdc2a2ebe7ab731fa0a7e92f14082d
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-11-26 17:57:01 +0200

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
 import burger from '../components/blocks/header/top/burger/burger.js';
 import order from '../components/blocks/header/order/order.js';
 import navProduct from '../components/blocks/header/nav-product/nav-product.js';
-import seeds from '../components/blocks/main/seeds/_seeds.js';
-import novelties from '../components/blocks/main/novelties/_novelties.js';
+import seeds from '../components/blocks/main/seeds/seeds.js';
+import novelties from '../components/blocks/main/novelties/novelties.js';
 import selctMenu from '../components/bem-blocks/drop-menu/select-menu/select-menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
