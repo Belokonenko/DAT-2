a062692a53cf3bc0ccc34e7bc17902ecc4903915
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-11-30 06:28:19 +0200

    Fix : nav-prod js

--- src/components/dev/section/header/nav-product/nav-product.js
+++ src/components/dev/section/header/nav-product/nav-product.js
@@ -1,34 +1,38 @@
 export default function navProduct() {
-    
     const navProductBtnArr = document.querySelector(".nav-product__btn-arr");
-    const navItems = document.querySelectorAll(".nav-product__item");
-    const navItemsHidenn = document.querySelectorAll(
-        ".nav-product__item-hidenn"
-    );
-    const navItemsHidennVisble = document.querySelectorAll(
-        ".nav-product__item-viseble"
-    );
 
-    navProductBtnArr.addEventListener("click", () => {
-        console.log("click btn");
+    if (navProductBtnArr) {
+        const navItems = document.querySelectorAll(".nav-product__item");
+        const navItemsHidenn = document.querySelectorAll(
+            ".nav-product__item-hidenn"
+        );
+        const navItemsHidennVisble = document.querySelectorAll(
+            ".nav-product__item-viseble"
+        );
 
-        navProductBtnArr.classList.toggle("nav-product__btn-arr--active")
+        navProductBtnArr.addEventListener("click", () => {
+            console.log("click btn");
 
-        navItemsHidenn.forEach((item) => {
-            item.classList.toggle("nav-product__item-viseble");
+            navProductBtnArr.classList.toggle("nav-product__btn-arr--active");
+
+            navItemsHidenn.forEach((item) => {
+                item.classList.toggle("nav-product__item-viseble");
+            });
         });
-    });
-    
-    //click btn close btn 
-    navItems.forEach((item) => {
-        item.addEventListener("click", (e) => {
-            if (!e.target.classList.contains("arr")) {
-                navItemsHidenn.forEach((item) => {
-                    item.classList.remove("nav-product__item-viseble");
-                });
-            
-            navProductBtnArr.classList.remove("nav-product__btn-arr--active")// roteate arr 0
-            }
+
+        //click btn close btn
+        navItems.forEach((item) => {
+            item.addEventListener("click", (e) => {
+                if (!e.target.classList.contains("arr")) {
+                    navItemsHidenn.forEach((item) => {
+                        item.classList.remove("nav-product__item-viseble");
+                    });
+
+                    navProductBtnArr.classList.remove(
+                        "nav-product__btn-arr--active"
+                    ); // roteate arr 0
+                }
+            });
         });
-    });
+    }
 }
