2d1cf7331669d54ff1cfdea92f459ad7969c2a3d
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-12-03 08:22:22 +0200

    dd

---
+++ src/modules/burger/burger.js
@@ -0,0 +1,15 @@
+
+export default function burgerCross() {
+    console.log("test");
+    const burgerBtn = document.querySelector(".burger-menu");
+     
+    console.log(burgerBtn);
+    // var burgerMenu = document.getElementById("burger-menu");
+
+    // var overlay = document.getElementById("menu");
+
+    // burgerMenu.addEventListener("click", function () {
+    //     this.classList.toggle("close");
+    //     overlay.classList.toggle("overlay");
+    // });
+}
